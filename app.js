03c47e1e4fedc0e956d33bc94c924be681ada059
Shirly28 <shirlymicaelas2004@gmail.com>
2026-05-10 21:13:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getFirestore, collection, getDocs, query, orderBy, doc, addDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, collection, getDocs, query, orderBy, doc, addDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
